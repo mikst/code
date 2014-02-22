98d698fe76b937957aac6e0e1186f4a5df1e46f1
mikst <mikst@mikst-2.local>
2014-02-22 22:06:32 +0900

    wishlab workshop example

--- wish_Lab/Button_Blink/Button_Blink.ino
+++ wish_Lab/Button_Blink/Button_Blink.ino
@@ -1,8 +1,8 @@
 /*
-  Button
+  Button Blink
  
- Turns on and off a light emitting diode(LED) connected to digital  
- pin 13, when pressing a pushbutton attached to pin 2. 
+ Blink light emitting diode(LED) connected to digital  
+ pin 9, when pressing a pushbutton attached to pin 2. 
  
  
  The circuit:
@@ -10,31 +10,31 @@
  * pushbutton attached to pin 2 from Ground
  
  
-
+------
  created 2005
  by DojoDave <http://www.0j0.org>
  modified 30 Aug 2011
  by Tom Igoe
- 
  This example code is in the public domain.
- 
+--------
+
  modified by KOBAKANT for the WishLab workshop
  http://www.kobakant.at/DIY/?p=4967
  
 */
 
-// constants won't change. They're used here to 
+
 // set pin numbers:
 int buttonPin = 2;     // the number of the pushbutton pin
 int ledPin =  9;      // the number of the LED pin
 
-// variables will change:
+// variables:
 int buttonState = 0;         // variable for reading the pushbutton status
 
 void setup() {
   // initialize the LED pin as an output:
   pinMode(ledPin, OUTPUT);      
-  // initialize the pushbutton pin as an input and turn on the pull up resister:
+  // initialize the pushbutton pin as an input and set the pull up resister:
   pinMode(buttonPin, INPUT_PULLUP);     
 }
 
@@ -43,7 +43,7 @@ void loop(){
   buttonState = digitalRead(buttonPin);
 
   // check if the pushbutton is pressed.
-  // if it is, the buttonState is HIGH:
+  // if it is, the buttonState is LOW:
   if (buttonState == LOW) {     
     // blink the LED:    
     digitalWrite(ledPin, HIGH);  
--- wish_Lab/Button_Fade/Button_Fade.ino
+++ wish_Lab/Button_Fade/Button_Fade.ino
@@ -1,7 +1,7 @@
 /*
-  Button
+  Button Fade
  
- Turns on and off a light emitting diode(LED) connected to digital  
+ Fede in/out the light emitting diode(LED) connected to digital  
  pin 9, when pressing a pushbutton attached to pin 2. 
  
  
@@ -10,34 +10,36 @@
  * pushbutton attached to pin 2 from Ground
  
  
-
+-------------
  created 2005
  by DojoDave <http://www.0j0.org>
  modified 30 Aug 2011
  by Tom Igoe
- 
  This example code is in the public domain.
+ ------------
+ 
  
  modified by KOBAKANT for the WishLab workshop
  http://www.kobakant.at/DIY/?p=4967
  
 */
 
-// constants won't change. They're used here to 
+
 // set pin numbers:
 int buttonPin = 2;     // the number of the pushbutton pin
 int ledPin =  9;      // the number of the LED pin
 
-// variables will change:
+// variables:
 int buttonState = 0;         // variable for reading the pushbutton status
 
-int delayAmount=30; // the amount of delay between each fading step
-int fadeAmount=5; // fading speed (size of the fading step)
+// the following two values will change the fading speed
+int delayAmount=30;     // the amount of delay between each fading step
+int fadeAmount=5;       // size of the fading steps
 
 void setup() {
   // initialize the LED pin as an output:
   pinMode(ledPin, OUTPUT);      
-  // initialize the pushbutton pin as an input and turn on the pull up resister:
+  // initialize the pushbutton pin as an input and set the pull up resister:
   pinMode(buttonPin, INPUT_PULLUP);     
 }
 
@@ -46,7 +48,7 @@ void loop(){
   buttonState = digitalRead(buttonPin);
 
   // check if the pushbutton is pressed.
-  // if it is, the buttonState is HIGH:
+  // if it is, the buttonState is LOW:
   if (buttonState == LOW) {     
   // fade in from min to max in increments of fadeAmount:
   for(int fadeValue = 0 ; fadeValue <= 255; fadeValue +=fadeAmount) { 
